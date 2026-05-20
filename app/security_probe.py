b61b178be655071fa3cfeecb114b7bf2bcb41124
Istiak <itsjoy003@gmail.com>
2026-05-21 00:32:08 +0600

    Add intentional GuardianCI security proof

---
+++ app/security_probe.py
@@ -0,0 +1,7 @@
+"""Intentional GuardianCI Phase 2 security proof.
+
+Do not merge this file. It exists only to prove GuardianCI blocks obvious
+hardcoded secrets before they reach main.
+"""
+
+DEMO_GEMINI_API_KEY = "AIzaSyAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
